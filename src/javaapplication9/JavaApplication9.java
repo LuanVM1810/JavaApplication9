33ccad8355efb81b53f56d92c11ad8c5bfc6627f
LuanVM1810 <Vomongluan@gmail.com>
2023-09-12 11:17:17 +0700

    upload 1st

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,11 @@
+
+package javaapplication9;
+
+
+public class JavaApplication9 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java, Github");
+    }
+    
+}
