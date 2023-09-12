d7e664af956f7bee010c1c43c214dff2a66c26fb
LuanVM1810 <Vomongluan@gmail.com>
2023-09-12 11:30:46 +0700

    update main() & add new readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+### Welcome to SWP391 Repository
+
+### You will find in this repo the following  stuff
+
+* A demo project written in Java
+(JSP/Servlet/Spring Boot)
+
+* ...
+
+#### Copyright 2023 © LuanVM1810 
--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -6,6 +6,7 @@ public class JavaApplication9 {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java, Github");
+        System.out.println("Học GitHub ở trường 3 chữ cũng nhàn");
     }
     
 }
